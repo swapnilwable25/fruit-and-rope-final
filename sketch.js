f9e8b449c197572c61dbaad14f053da9b9a459a6
rashmi-sathya <72431927+rashmi-sathya@users.noreply.github.com>
2021-07-27 18:59:39 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var holder,ball,ground;
+var stand1,stand2;
+var ball;
+var slingShot;
+var fruit;
+function preload(){
+ fruit=loadImage("melon.png");
+ g=loadImage("basket.png")
+}
+function setup() {
+  createCanvas(900,400);
+  engine = Engine.create();
+  world = engine.world;
+  Engine.run(engine);
+  ground = new Ground();
+  
+ 
+
+  //ball holder with slings
+  ball = Bodies.circle(50,200,20);
+  World.add(world,ball);
+
+  slingShot = new Slingshot(this.ball,{x:100,y:100});
+
+}
+function draw() {
+  background(56,44,44); 
+ 
+  //Engine.update(engine);
+  //text(mouseX + ',' + mouseY, 10, 15);
+  
+  ground.display();
+  g.scale=.025;
+
+
+  imageMode(CENTER)
+  image(fruit ,ball.position.x,ball.position.y,40,40);
+  image(g,450,270)
+
+  slingShot.display();
+}
+function mouseDragged(){
+  Matter.Body.setPosition(this.ball,{x:mouseX,y:mouseY});
+}
+function mouseReleased(){
+  slingShot.fly();
+
+}
